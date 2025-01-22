7df133cd8e5f901f48c49f5b4b91c2c2ed1bb91c
Goblnn <caasier42@gmail.com>
2025-01-22 16:50:54 -0700

    a2 update

--- S25_A2_MaterialSelection-student/student_work/submission.py
+++ S25_A2_MaterialSelection-student/student_work/submission.py
@@ -10,10 +10,32 @@ densities = input("DENSITIES>")
 density_indices = input("DENSITY_INDICES>")
 resists = input("RESISTS>")
 
+
+# Making into lists
 metal_list = metal_list.split(" ")
 density_indices = density_indices.split(" ")
 
+# List information
 metal_index = metal_list.index(metal)
-metal_price = metal_list[metal_index + 1]
-metal_density = densities[int(density_indices[0]):int(density_indices[1])+1]
+metal_price = int(metal_list[metal_index + 1])
+metal_density = int(densities[int(density_indices[0]):int(density_indices[1])+1])
 
+# Resistance Finding
+resist_index = resists.index(metal)
+metal_resist = int(resists[resist_index+len(metal):(resist_index+len(metal))+4])
+
+# Cable Information
+radius = .5
+length = 100 * 63360
+pi = 3.1415
+volume = pi * (radius ** 2) * length
+area = pi * (radius ** 2)
+
+# Final Calculations
+weight = volume * metal_density
+price = metal_price / weight
+resistivity = metal_resist * (length / area)
+
+print(f"OUTPUT {metal} Weight {weight}")
+print(f"OUTPUT {metal} Price {price}")
+print(f"OUTPUT {metal} Resistivity {resistivity}")
