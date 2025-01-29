c3b893a00100c5fa9bfa726f064fb5ebb4fd81c6
Goblnn <caasier42@gmail.com>
2025-01-29 16:15:26 -0700

    Update test.py

--- test.py
+++ test.py
@@ -1,3 +1,5 @@
-OUR_NUM= 3.14732
-print("%.3f" %OUR_NUM)
-print(f"OUTPUT> ", "%.3f" %OUR_NUM)
+my_numbers = ["1", "2", "3", "4"]
+if 2 in my_numbers:
+    print("2 is in my list!")
+else:
+    print("2 is not in my list!")
