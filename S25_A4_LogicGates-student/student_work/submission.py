a92bec690df6382d0caea8d495d861a9c40d8836
Goblnn <caasier42@gmail.com>
2025-02-10 23:08:44 -0700

    finish a4 and update test file

--- S25_A4_LogicGates-student/student_work/submission.py
+++ S25_A4_LogicGates-student/student_work/submission.py
@@ -4,9 +4,9 @@
 # References: no one
 # Time: 
 
-gate = input()
-input1 = int(input())
-input2 = int(input())
+gate = input("GATE> ")
+input1 = int(input("INP1> "))
+input2 = int(input("INP2> "))
 
 valid_gates = ["AND", "OR", "NAND", "NOR", "XOR", "XNOR"]
 
@@ -16,3 +16,26 @@ elif(input1 != 1 and input1 != 0):
     print(f"OUTPUT Invalid Input {input1}")
 elif(input2 != 1 and input2 != 0):
     print(f"OUTPUT Invalid Input {input2}")
+else:
+    if(input1 == 1):
+        x = True
+    else:
+        x = False
+
+    if(input2 == 1):
+        y = True
+    else:
+        y = False
+
+    if(gate == "AND"):
+        print(f"OUTPUT {x and y}")
+    elif(gate == "OR"):
+        print(f"OUTPUT {x or y}")
+    elif(gate == "NAND"):
+        print(f"OUTPUT {not (x and y)}")
+    elif(gate == "NOR"):
+        print(f"OUTPUT {not (x or y)}")
+    elif(gate == "XOR"):
+        print(f"OUTPUT {x != y}")
+    elif(gate == "XNOR"):
+        print(f"OUTPUT {x == y}")
--- test.py
+++ test.py
@@ -1,9 +1,4 @@
-import math
+x = 0
+y = 1
 
-old_x = 0
-new_x =3
-
-old_y = 0
-new_y = 4
-
-print(math.sqrt((new_x - old_x)**2 + (new_y - old_y)**2))
+print(x or y)
