689861f84095f2ef852a58aaca4dd2856c3543f2
Goblnn <caasier42@gmail.com>
2025-02-14 20:28:42 -0700

    a5 finish

--- S25_A5_RobotMovement-student/student_work/submission.py
+++ S25_A5_RobotMovement-student/student_work/submission.py
@@ -6,8 +6,8 @@
 
 import math
 
-bat = int(input())
-temp = int(input())
+bat = int(input("BATTERY> "))
+temp = int(input("HEAT> "))
 
 x_coord = 0
 y_coord = 0
@@ -15,27 +15,27 @@ y_coord = 0
 num_moves = 0
 
 while(bat > 10 and temp < 125):
-    inp = input()
+    inp = input("COORD> ")
 
     if(inp == "DONE"):
         break
 
     inp = inp.split()
 
-    x = int(inp[0])
-    y = int(inp[1])
+    x = float(inp[0])
+    y = float(inp[1])
 
     distance_moved = math.sqrt((x - x_coord)**2 + (y - y_coord)**2)
-    bat_change = abs(distance_moved // 2)
+    bat_change = int(abs(distance_moved // 2))
     temp_change = bat_change * 5
     
     bat -= bat_change
     temp += temp_change
 
-    x_coord += x
-    y_coord += y
+    x_coord = x
+    y_coord = y
 
-    print(f"OUTPUT Distance: {distance_moved:.2f} Battery: {bat:.2f}")
+    print(f"OUTPUT Distance: {distance_moved:.2f} Battery: {bat}")
     num_moves += 1
 
 print(f"OUTPUT {num_moves}")
