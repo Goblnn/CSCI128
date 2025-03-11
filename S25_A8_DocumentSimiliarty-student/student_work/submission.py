1fa3b8d82e15944036198424ac2ce8706b115074
Goblnn <caasier42@gmail.com>
2025-03-10 23:32:55 -0600

    a8 die

--- S25_A8_DocumentSimiliarty-student/student_work/submission.py
+++ S25_A8_DocumentSimiliarty-student/student_work/submission.py
@@ -4,3 +4,193 @@
 # References: None
 # Time: 2 hours
 
+import math
+
+def dot_product(v1, v2):
+    product = 0
+    for i in range(len(v1)):
+        product += v1[i] * v2[i]
+
+    return int(product)
+
+def magnitude(v):
+    sum = 0
+    for num in v:
+        sum += num**2
+
+    mag = math.sqrt(sum)
+    print(v)
+    print(mag)
+    return mag
+
+def cosine(v1, v2):
+    dot = dot_product(v1, v2)
+    mag1 = magnitude(v1)
+    mag2 = magnitude(v2)
+
+    if(mag1 == 0 or mag2 == 0):
+        return 0.00
+    else:
+        cosine = dot / (mag1 * mag2)
+
+        return cosine
+
+def words_to_vector(index_array, word_array):
+    frequency = []
+
+    for i in range(len(index_array)):
+        frequency.append(0) 
+
+    for line in word_array:
+        for i in range(len(line)):
+            if line[i] in index_array:
+                word_index = index_array.index(line[i])
+                frequency[word_index] += 1
+
+    return frequency 
+
+def join_frequency_vectors(fv1, fv2):
+    final = []
+
+    for i in range(len(fv1)):
+        final.append(fv1[i] + fv2[i])
+
+    return final
+
+def get_document():
+    docu = []
+    lines = int(input("NUM LINES> "))
+
+    for i in range(lines):
+        line = input(f"Line {i+1}> ")
+        line = line.split()
+        docu.append(line)
+
+    return docu
+
+def clean_document_line(line):
+    to_remove = ".,:;?!1234567890"
+    to_split = "-_"
+
+    i = 0
+    while i < len(line):
+        for char in to_remove:
+            line[i] = line[i].replace(char, "")
+
+        line[i] = line[i].replace('"', "")
+        line[i] = line[i].replace("'", "")
+        
+        for char in to_split:
+            if(char in line[i]):
+                temp = line[i].split(char)
+                for j in range(len(temp)):
+                    if(j == 0):
+                        line[i] = temp[j].lower()
+                    else:
+                        line.insert(i + j, temp[j].lower())
+
+        line[i] = line[i].lower()
+        i += 1
+
+    i = 0
+    while i < len(line):
+        word = line[i]  
+        if word == "":
+            line.pop(i)
+            if not line:
+                break
+        else:
+            i += 1
+
+    return line
+
+def create_index_array(doc_1, doc_2):
+    unique = []
+
+    for line in doc_1:
+        for word in line:
+            if word not in unique:
+                unique.append(word)
+
+    for line in doc_2:
+        for word in line:
+            if word not in unique:
+                unique.append(word)
+
+    return unique
+
+if __name__ == "__main__":
+    doc1 = get_document()
+    doc2 = get_document()
+
+    for i in range(len(doc1)):
+        doc1[i] = clean_document_line(doc1[i])
+
+    for i in range(len(doc2)):
+        doc2[i] = clean_document_line(doc2[i])
+    
+    print(doc1)
+    print(doc2)
+
+    empty = True
+    i = 0
+    while i < len(doc1):
+        for word in doc1[i]:
+            # print(word)
+            if word != "":
+                empty = False
+                # print("uh oh")
+            # print("")
+        
+        if empty == True:
+            doc1.pop(i)
+            if not doc1:
+                break
+        else:
+            i += 1
+        
+
+    empty = True
+    i = 0
+    while i < len(doc2):
+        for word in doc2[i]:
+            # print(word)
+            if word != "":
+                empty = False
+                # print("uh oh")
+            # print("")
+        
+        if empty == True:
+            doc2.pop(i)
+            if not doc2:
+                break
+        else:
+            i += 1
+
+    if doc1 == [] or doc2 == []:
+        print("OUTPUT ERROR: At least one document is empty!")
+    else:
+        index_array = create_index_array(doc1, doc2)
+
+        frequency1 = words_to_vector(index_array, doc1)
+        frequency2 = words_to_vector(index_array, doc2)
+        print(index_array)
+        print(doc1)
+        print(doc2)
+        print(frequency1)
+        print(frequency2)
+
+        frequency_final = join_frequency_vectors(frequency1, frequency2)
+
+        cos = cosine(frequency1, frequency2)
+
+        print(f"OUTPUT {cos:.2f}%")
+        
+        if(cos > .75):
+            print("OUTPUT Very Similar")
+        elif(cos > .5):
+            print("OUTPUT Similar")
+        elif(cos > .25):
+            print("OUTPUT Not Very Similar")
+        else:
+            print("OUTPUT Not Similar")
--- test.py
+++ test.py
@@ -1,6 +1,21 @@
-output = ""
-my_list = [1, 3, 0]
-for num in my_list:
-    for i in range(0, num):
-        output += f"{i} "
-print(output)
+doc1 = [[''], ['']]
+doc2 =  [['ants', 'like', 'apples']]
+
+empty = True
+i = 0
+while i < len(doc1):
+    print(f"start {doc1}")
+    for word in doc1[i]:
+        print(word)
+        if word != "":
+            empty = False
+            print("uh oh")
+        print("")
+    
+    if empty == True:
+        print(f"remove {doc1}")
+        doc1.pop(i)
+        if not doc1:
+            break
+    else:
+        i += 1
