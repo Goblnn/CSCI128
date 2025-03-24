5a208a958daceda00440abfbaacaaf924984166f
Goblnn <caasier42@gmail.com>
2025-03-24 16:45:28 -0600

    test updates

--- test.py
+++ test.py
@@ -1,21 +1,5 @@
-doc1 = [[''], ['']]
-doc2 =  [['ants', 'like', 'apples']]
-
-empty = True
-i = 0
-while i < len(doc1):
-    print(f"start {doc1}")
-    for word in doc1[i]:
-        print(word)
-        if word != "":
-            empty = False
-            print("uh oh")
-        print("")
-    
-    if empty == True:
-        print(f"remove {doc1}")
-        doc1.pop(i)
-        if not doc1:
-            break
-    else:
-        i += 1
+with open('file.txt' , 'r' ) as file_1:
+    for line in file_1:
+        print(line)
+    lines = file_1.readlines()
+    print(lines)
