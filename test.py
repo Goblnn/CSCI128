512cb9f2a1f28fade9e8d31d5eba2ac9fef5e55e
Goblnn <caasier42@gmail.com>
2025-03-31 16:31:23 -0600

    Update test.py

--- test.py
+++ test.py
@@ -1,2 +1,12 @@
-print()
-print("ah")
+import matplotlib.pyplot as plt
+
+categories = ["Red", "Orange", "Yellow", "Green", "Blue", "Indigo", "Violet"]
+votes = [20, 12, 14, 25, 29, 4, 9]
+colors = ["red", "orange", "yellow", "green", "blue", "indigo", "violet"] 
+
+plt.bar(categories, votes, color=colors) 
+plt.title("What is your favorite color?")
+plt.xlabel("Colors")
+plt.ylabel("Votes")
+
+plt.show()
