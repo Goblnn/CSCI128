623bc4536760449c77837d4d4fcf8f7a35589a3c
Goblnn <caasier42@gmail.com>
2025-04-13 16:59:23 -0600

    a12 some functions work
    WTF DOES MERGE LISTS DO ITS USELESS SO FAR

--- S25_A12_ImageCompression-student/student_work/submission.py
+++ S25_A12_ImageCompression-student/student_work/submission.py
@@ -7,10 +7,35 @@
 import helper_library as helper
 
 def block_average(grid, x, y, width, height):
+    new_grid = grid[y: y + height]
+    print(new_grid)
 
+    for i, line in enumerate(new_grid):
+        new_grid[i] = line[x: x + width]
+    print(new_grid)
+
+    merged_values = new_grid[0][0]
+
+    count = 1
+
+    for i, line in enumerate(new_grid):
+        for j, values in enumerate(line):
+            if i == 0 and j == 0:
+                continue
+            else:
+                count += 1
+                merged_values[0] = (merged_values[0] + values[0])
+                merged_values[1] = (merged_values[1] + values[1])
+                merged_values[2] = (merged_values[2] + values[2])
+
+    merged_values[0] = merged_values[0] // count
+    merged_values[1] = merged_values[1] // count
+    merged_values[2] = merged_values[2] // count
+
+    return merged_values
 
 def create_compressed_block(avg_color, width, height):
-
+    return 0
 
 def merge_lists(lst1, lst2):
     new_lst = []
