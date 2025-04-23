7fbeb81327a654e9c896639626bca7043aeabab7
Goblnn <caasier42@gmail.com>
2025-04-23 16:16:06 -0600

    asdad

--- test.py
+++ test.py
@@ -1,18 +1,3 @@
-class Animal:
-    def __init__(self, species, name):
-        self.species = species
-        self.name = name
-
-
-zoo = {}
-simba = Animal("Lion" , "Simba" )
-nala = Animal("Lion" , "Nala" )
-timon = Animal("Meerkat" ,"Timon" )
-pumbaa = Animal("Warthog" ,"Pumbaa" )
-zoo[simba.species] = simba
-zoo[nala.species] = nala
-zoo[timon.species] = timon
-zoo[pumbaa.species]=pumbaa
-
-for animal in zoo.values():
-    print(animal.name)
+my_dict = {1: 4, 2: 5, 3: 6}
+for mystery in my_dict:
+    print(mystery, end=" ")
